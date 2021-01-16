6028fb259c454178cb3a02cf09b57a8fd46d2810
lucassherwin <lksherwin@gmail.com>
2021-01-16 16:38:10 -0500

    updated posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,14 +5,26 @@ class PostsController < ApplicationController
     end
 
     def create
-
+        post = Post.create(post_params)
+        render json: post
     end
 
     def show
-
+        post = Post.find_by(title: params[:title])
+        if post.user_id == params[:user_id]
+            render json: post
+        else
+            render json: {message: "You do not have access to this post"}
+        end
     end
 
     def update
-        
+
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :description, :points, :tag, :user_id)
     end
 end
