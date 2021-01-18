ff5f166f793c16599bc766711f55fae700cbf117
lucassherwin <lksherwin@gmail.com>
2021-01-18 12:26:04 -0500

    route for create account

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,14 @@ class PostsController < ApplicationController
         posts = Post.all
         posts_arr = []
         posts.each do |post|
-            images = [] # store all the images
-            post.images.each do |i|
-                image = rails_blob_path(i) # get the image
-                images << image # store it in the images array
+            if !post.private # the post is not private
+                images = [] # store all the images
+                post.images.each do |i|
+                    image = rails_blob_path(i) # get the image
+                    images << image # store it in the images array
+                end
+                posts_arr << {post: post, images: images} # store post with images in the array
             end
-            posts_arr << {post: post, images: images} # store post with images in the array
         end
         render json: posts_arr
     end
@@ -20,12 +22,6 @@ class PostsController < ApplicationController
 
     def show
         posts = Post.where(user_id: params[:user_id]) # find all the posts by the user_id
-        # if post.user_id == params[:user_id] # if the post has the same user_id as the user attempting to access it
-        #     render json: post # render it
-        # else
-        #     render json: {message: "You do not have access to this post"} # otherwise let the user know they do not have permission
-        # end
-
         posts_arr = []
         posts.each do |post| # for each post
             # byebug
@@ -50,6 +46,6 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:title, :description, :points, :tag, :private :user_id)
+        params.require(:post).permit(:title, :description, :points, :tag, :private, :user_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   post '/posts/:user_id', to: 'posts#show'
 
   post '/rails/active_storage/direct_uploads', to: 'direct_uploads#create'
+
+  post '/createuser', to: 'users#create' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
