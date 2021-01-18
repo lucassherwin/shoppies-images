8b4ff9f3e8e382edbf8cc73ca6986bb21fecffcc
lucassherwin <lksherwin@gmail.com>
2021-01-18 16:46:43 -0500

    changes

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -1,19 +1,19 @@
 class DirectUploadsController < ActiveStorage::DirectUploadsController
     skip_before_action :verify_authenticity_token
 
-    # def create
-    #     blob = ActiveStorage::Blob.create_before_direct_upload!(blob_args)
-    #     render json: direct_upload_json(blob)
-    # end
+    def create
+        blob = ActiveStorage::Blob.create_before_direct_upload!(blob_args)
+        render json: direct_upload_json(blob)
+    end
 
-    # private
+    private
 
-    # def direct_upload_json(blob)
-    #     blob.as_json(root: false, methods: :signed_id).merge(service_url: url_for(blob)).merge(direct_upload: {
-    #         url: blob.service_url_for_direct_upload,
-    #         headers: blob.service_headers_for_direct_upload
-    #     })
-    # end
+    def direct_upload_json(blob)
+        blob.as_json(root: false, methods: :signed_id).merge(service_url: url_for(blob)).merge(direct_upload: {
+            url: blob.service_url_for_direct_upload,
+            headers: blob.service_headers_for_direct_upload
+        })
+    end
 
     # from https://gist.github.com/elliott-king/12bc6c9ff9a69b5f04d74ebb263ba702
     # def create
