1a72a5d848d0f58838695322102a122e7797959f
lucassherwin <lksherwin@gmail.com>
2021-01-19 13:29:49 -0500

    removed posts from seeds for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,39 +9,39 @@
 User.destroy_all
 Post.destroy_all
 
-user = User.create(username: 'test', password: 'test')
-post = Post.create(
-    title: 'test title',
-    description: 'test description',
-    points: 5,
-    private: false,
-    tag: 'test',
-    user_id: user.id
-)
+# user = User.create(username: 'test', password: 'test')
+# post = Post.create(
+#     title: 'test title',
+#     description: 'test description',
+#     points: 5,
+#     private: false,
+#     tag: 'test',
+#     user_id: user.id
+# )
 
-post.images.attach(
-    io: File.open('./public/images/shrek.png'),
-    filename: 'shrek.png',
-    content_type: 'application/png'
-)
+# post.images.attach(
+#     io: File.open('./public/images/shrek.png'),
+#     filename: 'shrek.png',
+#     content_type: 'application/png'
+# )
 
-post_2 = Post.create(
-    title: 'test title 2',
-    description: 'test description 2',
-    points: 5684,
-    private: false,
-    tag: 'test',
-    user_id: user.id
-)
+# post_2 = Post.create(
+#     title: 'test title 2',
+#     description: 'test description 2',
+#     points: 5684,
+#     private: false,
+#     tag: 'test',
+#     user_id: user.id
+# )
 
-post_2.images.attach(
-    io: File.open('./public/images/shrek2.jpg'),
-    filename: 'shrek2.jpg',
-    content_type: 'application/jpg'
-)
+# post_2.images.attach(
+#     io: File.open('./public/images/shrek2.jpg'),
+#     filename: 'shrek2.jpg',
+#     content_type: 'application/jpg'
+# )
 
-post_2.images.attach(
-    io: File.open('./public/images/shock.png'),
-    filename: 'shock.png',
-    content_type: 'application/png'
-)
+# post_2.images.attach(
+#     io: File.open('./public/images/shock.png'),
+#     filename: 'shock.png',
+#     content_type: 'application/png'
+# )
